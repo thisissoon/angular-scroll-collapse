a6d0dedb0190edf79144c28039acca4a6dd30b83
Edward Opare-Aryee <ed@thisissoon.com>
2016-06-13 12:58:05 +0100

    Release v0.1.1

--- dist/angular-smart-nav.js
+++ dist/angular-smart-nav.js
@@ -1,4 +1,4 @@
-/*! angular-smart-nav - v0.1.0 - 2016-06-10 */
+/*! angular-smart-nav - v0.1.1 - 2016-06-13 */
 'use strict';
 /**
  * Module that detects the last scroll direction and
@@ -38,7 +38,6 @@ angular.module('sn.smartNav', [
   function ($window, $document){
     return {
       restrict: 'A',
-      scope: {},
       link: function($scope, $element){
         /**
          * The last recorded scrollTop position
@@ -54,6 +53,23 @@ angular.module('sn.smartNav', [
          * @type     {Boolean}
          */
         var scrollingDown = false;
+        /**
+         * @method getScrollTop
+         * @returns {Number} the number of pixel from top of
+         *                   page the user has scrolled
+         */
+        var getScrollTop = function getScrollTop() {
+          var doc = $document[0].documentElement,
+              body = $document[0].body,
+              scrollTop = ( (doc && doc.scrollTop) || (body && body.scrollTop) || 0 );
+
+          return scrollTop;
+        };
+        /**
+         * The number of pixels from the top of the page
+         * @property {Number} positionFromTop
+         */
+        var positionFromTop = (getScrollTop() + $element[0].getBoundingClientRect().top);
         /**
          * @method isScrollingDown
          * @private
@@ -108,9 +124,8 @@ angular.module('sn.smartNav', [
          * @private
          * @method calAffixedMode
          */
-        var calAffixedMode = function calAffixedMode(){
-          var positionFromTop = $element[0].getBoundingClientRect().top;
-          if (positionFromTop <= 0 ) {
+        var calAffixedMode = function calAffixedMode(scrollTop){
+          if (scrollTop >= positionFromTop ) {
             $element.addClass('affix');
           } else {
             $element.removeClass('affix');
@@ -124,13 +139,11 @@ angular.module('sn.smartNav', [
          * @method onScroll
          */
         var onScroll = function onScroll() {
-          var doc = $document[0].documentElement,
-              body = $document[0].body,
-              scrollTop = ( (doc && doc.scrollTop) || (body && body.scrollTop) || 0 );
+          var scrollTop = getScrollTop();
 
           calScrollDir(scrollTop);
           calMinimisedMode(scrollTop);
-          calAffixedMode();
+          calAffixedMode(scrollTop);
 
           lastScrollTop = scrollTop;
         };
@@ -144,6 +157,8 @@ angular.module('sn.smartNav', [
 
         $scope.$on('$destroy', onDestroy);
         angular.element($window).on('scroll', onScroll);
+
+        onScroll();
       }
     };
   }
