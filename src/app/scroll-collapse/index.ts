cbc473c6a74cf0641278e56d23343d960b4b5de1
Edward Opare-Aryee <ed@thisissoon.com>
2017-12-01 14:51:31 +0000

    feat(build): Move build and dev environment to angular cli and ng-packagr
    Simplified the dev and build environment to be based on the cli

---
+++ src/app/scroll-collapse/index.ts
@@ -0,0 +1,3 @@
+export * from './scroll-collapse.directive';
+export * from './scroll-collapse.module';
+export * from './shared';
---
+++ src/app/scroll-collapse/shared/index.ts
@@ -0,0 +1,4 @@
+export * from './classes';
+export * from './direction.enum';
+export * from './event-data';
+export * from './viewport.model';
