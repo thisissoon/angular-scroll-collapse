0b56342ec17a38fa121930ec57dbbbc1f9fd6c64
Ed Opare-Aryee <ed@thisissoon.com>
2017-12-04 11:38:57 +0000

    fix(build): generate correct metadata needed for ng-language-service

--- index.ts
+++ index.ts
@@ -1,1 +1,6 @@
-export * from './src/app/scroll-collapse';
+export { ScrollCollapseModule } from './src/app/scroll-collapse/scroll-collapse.module';
+export { ScrollCollapseDirective } from './src/app/scroll-collapse/scroll-collapse.directive';
+export { affixClass, directionDownClass, directionUpClass, minimiseClass } from './src/app/scroll-collapse/shared/classes';
+export { Direction } from './src/app/scroll-collapse/shared/direction.enum';
+export { eventLoad, eventPathLoadScroll, eventPathResize, eventResize, eventScroll } from './src/app/scroll-collapse/shared/event-data';
+
